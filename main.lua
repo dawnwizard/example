b8f9b08fd40889d27359fd4a89dc4d118ed5303e
dawnwizard <948507867@qq.com>
2018-01-11 16:46:51 +0800

    0.02

--- main.lua
+++ main.lua
@@ -5,9 +5,13 @@ skynet.start(function()
     -- 启动函数里调用Skynet API开发各种服务
     print("======Server start=======")
     -- skynet.newservice(name, ...)启动一个新的 Lua 服务(服务脚本文件名)
-    skynet.newservice("service1")
+
+    local service2 = skynet.newservice("service2")
+    -- 向service2服务发出请求，设置key1的值
+    skynet.call(service2,"lua","set","key1","value111111")  
+    -- 向service2服务发出请求，获取key1的值
+    local kv = skynet.call(service2,"lua","get","key1")
+    print(kv)
 
     -- 退出当前的服务
-    -- skynet.exit 之后的代码都不会被运行。而且，当前服务被阻塞住的 coroutine 也会立刻中断退出。
-    skynet.exit()
 end)
