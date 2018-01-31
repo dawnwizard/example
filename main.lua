eede41ce0de13c5164fb36500d02cdb30378262c
dawnwizard <948507867@qq.com>
2018-01-31 11:25:35 +0800

    1.20

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ skynet.start(function()
     -- 启动函数里调用Skynet API开发各种服务
     print("======Server start=======")
 
-    skynet.newservice("testwebsocket")
+    -- skynet.newservice("testwebsocket")
+    skynet.newservice("mgr_server")
     skynet.exit()
 end)
